edef58b3cb66744fc76e0fdb14a61c77c5807fe5
matt-han <matthansert@gmail.com>
2013-09-23 21:27:02 +0200

    formatting

--- BaseWindow.h
+++ BaseWindow.h
@@ -25,77 +25,64 @@ template <class DERIVED_TYPE>
 class BaseWindow
 {
 public:
-
-
 	//Windows default STATIC message handler
 	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-    {
-        DERIVED_TYPE *pThis = NULL;
+	{
+		DERIVED_TYPE *pThis = NULL;
 
-		
-
-        if (uMsg == WM_NCCREATE)
-        {
+		if (uMsg == WM_NCCREATE)
+		{
 			//extract pointer
-            CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
-            pThis = (DERIVED_TYPE*)pCreate->lpCreateParams;
-            SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
+			CREATESTRUCT* pCreate = (CREATESTRUCT*)lParam;
+			pThis = (DERIVED_TYPE*)pCreate->lpCreateParams;
+			SetWindowLongPtr(hwnd, GWLP_USERDATA, (LONG_PTR)pThis);
 
-            pThis->m_hwnd = hwnd;
-        }
-        else
-        {
-            pThis = (DERIVED_TYPE*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
-        }
-        if (pThis)
-        {
-            return pThis->HandleMessage(uMsg, wParam, lParam);
-        }
-        else
-        {
-            return DefWindowProc(hwnd, uMsg, wParam, lParam);
-        }
-    }//WindowProc
+			pThis->m_hwnd = hwnd;
+		}
+		else
+			pThis = (DERIVED_TYPE*)GetWindowLongPtr(hwnd, GWLP_USERDATA);
+
+		if (pThis)
+			return pThis->HandleMessage(uMsg, wParam, lParam);
+		else
+			return DefWindowProc(hwnd, uMsg, wParam, lParam);
+	}//WindowProc
 
     
 	BaseWindow() : m_hwnd(NULL) { }
 
-    BOOL Create(
-        LPCSTR lpWindowName,
-        DWORD dwStyle,
-        DWORD dwExStyle = 0,
-        int x = WINDOW_X,
-        int y = WINDOW_Y,
-        int nWidth = WIN_WIDTH,
-        int nHeight = WIN_HEIGHT,
-        HWND hWndParent = 0,
-        HMENU hMenu = 0
-        )
+	BOOL Create(LPCSTR lpWindowName,
+				DWORD dwStyle,
+				DWORD dwExStyle = 0,
+				int x = WINDOW_X,
+				int y = WINDOW_Y,
+				int nWidth = WIN_WIDTH,
+				int nHeight = WIN_HEIGHT,
+				HWND hWndParent = 0,
+				HMENU hMenu = 0)
     {
 		//Window properties
-        WNDCLASS wc = {0};
+		WNDCLASS wc = {0};
 		
 		//wc.lpszClassName = L"Serial Port Tester";
 		wc.hbrBackground = GetSysColorBrush(COLOR_3DFACE);
 		wc.hCursor       = LoadCursor(0, IDC_ARROW);
 		wc.hIcon		 = LoadIcon(NULL, IDI_APPLICATION);
 		wc.lpfnWndProc   = DERIVED_TYPE::WindowProc;
-        wc.hInstance     = GetModuleHandle(NULL);
-        wc.lpszClassName =  ClassName();
+		wc.hInstance     = GetModuleHandle(NULL);
+		wc.lpszClassName =  ClassName();
 
-        RegisterClass(&wc);
+		RegisterClass(&wc);
 
 		//main window creation and handle
-        m_hwnd = CreateWindowEx(
-            dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
-            nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this
-
-			);
+		m_hwnd = CreateWindowEx(
+			dwExStyle, ClassName(), lpWindowName, dwStyle, x, y,
+			nWidth, nHeight, hWndParent, hMenu, GetModuleHandle(NULL), this);
 
         return (m_hwnd ? TRUE : FALSE);
     }//create
 
-    HWND window() const { return m_hwnd;}
+	HWND window() const { return m_hwnd;}
 
 protected:
 
